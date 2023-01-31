c0da214fd79f2cb11fd1b5584a7b3f66bd8b6149
Miryru <102163275+Miryru@users.noreply.github.com>
2023-01-31 17:11:42 +0100

    if per i tre casi difersi

--- ARP-ThirdAssignment/src/processA.c
+++ ARP-ThirdAssignment/src/processA.c
@@ -1,4 +1,5 @@
 #include "./../include/processA_utilities.h"
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +14,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
 // Declaration variables for bmp file
 const int width = 1600;
@@ -89,6 +94,23 @@ void cancel_blue_circle(int radius,int x,int y, bmpfile_t *bmp) {
 
 int main(int argc, char *argv[])
 {
+    // Variable initialization for a server socket connection
+    // int sockfd, newsockfd, portno, clilen, n; 
+    // char buffer[256];
+
+    // struct in_addr {
+    //     in_addr_t s_addr;   // 32-bit IPv4 addresses
+    // };
+
+    // struct sockaddr_in serv_addr, cli_addr;
+    // struct sockaddr_in {
+    //     short sin_family;   // e.g. AF_INET
+    //     u_short sin_port;   // e.g. TCP/UDP Port num
+    //     struct in_addr sin_addr;    // IPv4 address
+    //     char sin_zero[8];   // unused
+    // }; 
+
+
     // Open the log file
     if ((log_fd = open("processA.log",O_WRONLY|O_APPEND|O_CREAT, 0666)) == -1)
     {
@@ -97,12 +119,13 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    printf("Choose the execution modality: \n");
-    printf("1. Normal: \n");
-    printf("2. Server: \n");
-    printf("3. Client: \n");
+    // Control to choose the execution mode
+    printf("Choose the execution mode: \n");
+    printf("Write 1 for Normal mode: \n");
+    printf("Write 2 for Server mode: \n");
+    printf("Write 3 Client mode: \n");
 
-    int modality;
+    int mode;
     char input[100];
     
     while (1) {
@@ -118,17 +141,17 @@ int main(int argc, char *argv[])
         }
         
         if (valid) {
-            modality = atoi(input);
-            if (modality >= 1 && modality <= 3) {
+            mode = atoi(input);
+            if (mode >= 1 && mode <= 3) {
                 break; 
             } 
             else {
-                printf("The number is not between 1 and 3. Try again.\n");
+                printf("Invalid input! The number is not 1 or 2 or 3. Try again.\n");
             }
         } 
         
         else {
-            printf("Invalid input. Try again.\n");
+            printf("Invalid input! The number is not 1 or 2 or 3. Try again.\n");
         }
     }
 
@@ -139,15 +162,9 @@ int main(int argc, char *argv[])
     info = localtime(&rawtime);
 
     // Write to the log file
-    sprintf(log_buffer, "<Process_A> Modality: %d %s\n", modality, asctime(info));   // Get the time
+    sprintf(log_buffer, "<Process_A> Modality: %d %s\n", mode, asctime(info));   // Get the time
     check = write(log_fd, log_buffer, strlen(log_buffer));  // Write to the log file
     CheckCorrectness(check);    // Check if the write is correct
-
-    // Utility variable to avoid trigger resize event on launch
-    int first_resize = TRUE;
-
-    // Initialize UI
-    init_console_ui();
 
     // Delcare circle radius
     int radius = 30;
@@ -191,91 +208,96 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    if (mode == 1){
+        // Utility variable to avoid trigger resize event on launch
+        int first_resize = TRUE;
 
-    // Infinite loop
-    while (TRUE)
-    {
-        // Get current time
-        time(&rawtime);
-        info = localtime(&rawtime);
+        // Initialize UI
+        init_console_ui();
 
-        // Get input in non-blocking mode
-        int cmd = getch();
-
-        int x = circle.x;   // Get the x coordinate of the circle
-        int y = circle.y;   // Get the y coordinate of the circle
-
-        // If user resizes screen, re-draw UI...
-        if(cmd == KEY_RESIZE) {
-            if(first_resize) {
-                first_resize = FALSE;   // Avoid trigger resize event on launch
-            }
-            else {
-                reset_console_ui(); // Re-draw UI
-            }
-        }
-
-        // Else, if user presses print button...
-        else if(cmd == KEY_MOUSE) {
-            if(getmouse(&event) == OK) {
-                if(check_button_pressed(print_btn, &event)) {
-                    mvprintw(LINES - 1, 1, "Print button pressed"); // Print a message on the screen
-
-                    // Write to the log file
-                    sprintf(log_buffer, "<Process_A> Print button pressed: %s\n", asctime(info));   // Get the time
-                    check = write(log_fd, log_buffer, strlen(log_buffer));  // Write to the log file
-                    CheckCorrectness(check);    // Check if the write is correct
-
-                    bmp_save(bmp, "out/image.bmp"); // Save the bmp file
-
-                    refresh();
-                    sleep(1);
-                    for(int j = 0; j < COLS - BTN_SIZE_X - 2; j++) {
-                        mvaddch(LINES - 1, j, ' '); // Clear the message on the screen
-                    }
-                }
-            }
-        }
-        
-        // If input is an arrow key, move circle accordingly...
-        else if(cmd == KEY_LEFT || cmd == KEY_RIGHT || cmd == KEY_UP || cmd == KEY_DOWN) 
+        // Infinite loop
+        while (TRUE)
         {
-            
-            sem_wait(semaphore);    // Wait for the semaphore
-            
-            // Write to the log file
-            sprintf(log_buffer, "<Process_A> Keyboard button pressed: %s\n", asctime(info));    // Get the time
-            check = write(log_fd, log_buffer, strlen(log_buffer));  // Write to the log file
-            CheckCorrectness(check);    // Check if the write is correct
-            
-            move_circle(cmd);   // Move the circle
-            draw_circle();  // Draw the circle
-    
-            cancel_blue_circle(radius,x,y, bmp);    // Cancel the circle
-            for (int i = 0; i < 1600; i++) {
-                for (int j = 0; j < 600; j++) {
-                    ShmPTR->m[i][j] = 0;    // Set the shared memory to 0
+            // Get current time
+            time(&rawtime);
+            info = localtime(&rawtime);
+
+            // Get input in non-blocking mode
+            int cmd = getch();
+
+            int x = circle.x;   // Get the x coordinate of the circle
+            int y = circle.y;   // Get the y coordinate of the circle
+
+            // If user resizes screen, re-draw UI...
+            if(cmd == KEY_RESIZE) {
+                if(first_resize) {
+                    first_resize = FALSE;   // Avoid trigger resize event on launch
                 }
-            }
-            
-            draw_blue_circle(radius,circle.x,circle.y, bmp);    // Draw the circle
-            
-            // Write to the shared memory
-            for (int i = 0; i < 1600; i++) {
-                for (int j = 0; j < 600; j++) {
-                    rgb_pixel_t *pixel = bmp_get_pixel(bmp, i, j);  // Get the pixel
-                    
-                    // If the pixel is blue, set the shared memory to 1
-                    if ((pixel->blue == 255) && (pixel->red == 0) && (pixel->green==0) && (pixel->alpha==0)) {
-                        ShmPTR->m[i][j] = 1;
-                    }
+                else {
+                    reset_console_ui(); // Re-draw UI
                 }
             }
 
-            sem_post(semaphore2);     // Post the semaphore  
-        }
+            // Else, if user presses print button...
+            else if(cmd == KEY_MOUSE) {
+                if(getmouse(&event) == OK) {
+                    if(check_button_pressed(print_btn, &event)) {
+                        mvprintw(LINES - 1, 1, "Print button pressed"); // Print a message on the screen
+
+                        // Write to the log file
+                        sprintf(log_buffer, "<Process_A> Print button pressed: %s\n", asctime(info));   // Get the time
+                        check = write(log_fd, log_buffer, strlen(log_buffer));  // Write to the log file
+                        CheckCorrectness(check);    // Check if the write is correct
+
+                        bmp_save(bmp, "out/image.bmp"); // Save the bmp file
+
+                        refresh();
+                        sleep(1);
+                        for(int j = 0; j < COLS - BTN_SIZE_X - 2; j++) {
+                            mvaddch(LINES - 1, j, ' '); // Clear the message on the screen
+                        }
+                    }
+                }
+            }
             
-    }
+            // If input is an arrow key, move circle accordingly...
+            else if(cmd == KEY_LEFT || cmd == KEY_RIGHT || cmd == KEY_UP || cmd == KEY_DOWN) 
+            {
+                
+                sem_wait(semaphore);    // Wait for the semaphore
+                
+                // Write to the log file
+                sprintf(log_buffer, "<Process_A> Keyboard button pressed: %s\n", asctime(info));    // Get the time
+                check = write(log_fd, log_buffer, strlen(log_buffer));  // Write to the log file
+                CheckCorrectness(check);    // Check if the write is correct
+                
+                move_circle(cmd);   // Move the circle
+                draw_circle();  // Draw the circle
+        
+                cancel_blue_circle(radius,x,y, bmp);    // Cancel the circle
+                for (int i = 0; i < 1600; i++) {
+                    for (int j = 0; j < 600; j++) {
+                        ShmPTR->m[i][j] = 0;    // Set the shared memory to 0
+                    }
+                }
+                
+                draw_blue_circle(radius,circle.x,circle.y, bmp);    // Draw the circle
+                
+                // Write to the shared memory
+                for (int i = 0; i < 1600; i++) {
+                    for (int j = 0; j < 600; j++) {
+                        rgb_pixel_t *pixel = bmp_get_pixel(bmp, i, j);  // Get the pixel
+                        
+                        // If the pixel is blue, set the shared memory to 1
+                        if ((pixel->blue == 255) && (pixel->red == 0) && (pixel->green==0) && (pixel->alpha==0)) {
+                            ShmPTR->m[i][j] = 1;
+                        }
+                    }
+                }
+
+                sem_post(semaphore2);     // Post the semaphore  
+            }   
+        }
 
         sem_close(semaphore);   // Close the semaphore
         sem_unlink("/mysem");   // Unlink the semaphore
@@ -288,6 +310,17 @@ int main(int argc, char *argv[])
 
         // Close the log file
         close(log_fd);
+    }
+    // else if (mode == 2){
+    //     printf("Process 2\n");
+    // }
+    // else if (mode == 3){
+    //     printf("Process 3\n");
+    // }
 
-        return 0;
+    while (TRUE){
+        printf("Process terminated\n");
+
+    
+    return 0;
 }
