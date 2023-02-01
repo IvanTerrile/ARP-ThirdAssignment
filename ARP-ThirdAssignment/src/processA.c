68753d6d9bd106332f22567414430a5b0af00107
Miryru <102163275+Miryru@users.noreply.github.com>
2023-02-01 12:21:01 +0100

    piccole modifiche insignificanti

--- ARP-ThirdAssignment/src/processA.c
+++ ARP-ThirdAssignment/src/processA.c
@@ -424,9 +424,7 @@ int main(int argc, char *argv[])
 
         bzero((char *) &serv_addr, sizeof(serv_addr));
         serv_addr.sin_family = AF_INET;
-        bcopy((char *)server->h_addr,
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
+        bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
         serv_addr.sin_port = htons(portno); 
 
         if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
