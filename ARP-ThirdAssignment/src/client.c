12b51235f144e837b2fd406410f6be68bf7e94cb
alemuraa <93042208+alemuraa@users.noreply.github.com>
2023-02-07 17:23:02 +0100

    h

--- ARP-ThirdAssignment/src/client.c
+++ ARP-ThirdAssignment/src/client.c
@@ -22,15 +22,16 @@ int main(int argc, char *argv[])
     struct hostent *server;
 
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
+    // if (argc < 3) {
+    //    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    //    exit(0);
+    // }
+    portno = 4000;//atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    char ip ="171.0.0.1";
+    server = gethostbyname(ip);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
