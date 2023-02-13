e411b39e6e986849fff3f506e44e6c5e7de9f374
alemuraa <93042208+alemuraa@users.noreply.github.com>
2023-02-13 15:32:09 +0100

    compile

--- ARP-ThirdAssignment/compile.sh
+++ ARP-ThirdAssignment/compile.sh
@@ -1,11 +1,6 @@
 # Compile process A
 gcc src/processA.c -lpthread -lncurses -lbmp -lm -o bin/processA &
-
 # Compile process B
 gcc src/processB.c -lpthread -lncurses -lbmp -lm -o bin/processB &
-
 # Compile master process
 gcc src/master.c -o bin/master
-
-
-
